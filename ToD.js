11eb68141825cb236d751987a20d4d8653a6abb1
JoelSRJE <JoelSRJE@hotmail.com>
2023-06-17 21:40:59 +0200

    added js for time&date etc

---
+++ ToD.js
@@ -0,0 +1,47 @@
+function updateDay() {
+    let today = new Date();
+    let day = today.getDay();
+    let dayList = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday ",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  document.getElementById("today").innerText = dayList[day];
+}
+
+
+function displayTime() {
+    let time = new Date();
+    let hour = time.getHours();
+    let minutes = time.getMinutes();
+
+    hour = hour < 10 ? "0" + hour : hour;
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+
+    let clock = hour + ":" + minutes;
+
+    document.getElementById("time").innerText = clock;
+}
+
+function displayDate() {
+    const date = new Date();
+    let day = date.getDate();
+    let month = date.getMonth() + 1;
+    let year = date.getFullYear();
+
+    let currentDate = year + " / " + month + " / " +day;
+
+    document.getElementById("date").innerText = currentDate;
+}
+
+  // Updates time
+setInterval(displayTime, 1000);
+
+updateDay();
+displayTime();
+displayDate();
